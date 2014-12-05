eabd098a0843c67f9aed0c77fadbcb9eccabff8b
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-12-05 16:13:47 +0100

    moving variables

--- public/js/scripts/indexpage.js
+++ public/js/scripts/indexpage.js
@@ -1,16 +1,17 @@
-$(document).ready(function(){
-    loadNewCatchphrase()
-});
-
 var catchphrases = [
     'to err is human; to emoji, divine',
     'an unemoji\'d life is not worth living',
     'fiat emojis',
     'give me emojis or give me death',
     'a man without emojis is a wild beast loosed upon this world',
-    'know thy emoji',
-    ''
+    'know thy emoji'
 ]
+
+$(document).ready(function(){
+    loadNewCatchphrase()
+});
+
+
 
 function loadNewCatchphrase(){
     var index = Math.floor(Math.random() * catchphrases.length)
