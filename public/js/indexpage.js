f25a51aa33d4c06355649407f52d1ab7670a228c
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-12-29 16:13:25 +0100

    mainpage styling

--- public/js/indexpage.js
+++ public/js/indexpage.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("#moreinfo").click(function() {
     $('html, body').animate({
-        scrollTop: $("#visionpage").offset().top - 50
+        scrollTop: $("#visionpage").offset().top - 20
     }, 2000);
 });
 
@@ -10,20 +10,20 @@ $(document).ready(function(){
     $(".navbar-nav").children('li').removeClass('active')
     var scrollTo = "#" + $(this).attr('data-scroll')
      $('html, body').animate({
-        scrollTop: $(scrollTo).offset().top - 50
+        scrollTop: $(scrollTo).offset().top - 20
     }, 2000);
   })
 
    $('#main-anchor').on('click', 'a', function(e){
     e.preventDefault();
      $('html, body').animate({
-        scrollTop: $("#splash").offset().top - 50
+        scrollTop: $("#splash").offset().top - 20
     }, 2000);
   })
 
    $('body').scrollspy({
       target:'.navbar-collapse',
-      offset: 60
+      offset: 50
     })
 
 });
